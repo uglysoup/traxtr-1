a389ce7d939da4b60223cfb23e3aa2770168ec91
vineeth <vineethnreddy@gmail.com>
2022-04-26 19:16:41 +0530

    user agent list added

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import requests
 import datetime
 import csv
+import random
 from bs4 import BeautifulSoup
 from proxyreq import *
 
+user_agent_list = [
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
+'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
+'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36','Mozilla/5.0 (X11; Linux x86_64; rv:     99.0) Gecko/20100101 Firefox/99.0',
+]
+for i in range(1,4):
+    user_agent = random.choice(user_agent_list)
 proxy = choice(get_working_proxies())
 product_url = input("Enter the url")
-headers = {"user agent":"Mozilla/5.0 (X11; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0"}
+headers = {"user agent":user_agent}
 page = requests.get(url=product_url, headers=headers)
 soup = BeautifulSoup(page.content,'lxml')
 #print(soup.prettify())
